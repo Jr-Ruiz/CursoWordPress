166a169dbc94e2f57c14da51d36bb9f71f94ec95
José Ramón Ruiz <Jr-Ruiz@users.noreply.github.com>
2016-09-03 08:45:22 +0200

    Create film.php

---
+++ HTML/film.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<meta name="description" content='Web de muestra para el curso de Wordpress'/>
+
+	<title>I Festival de Cine Ínsula de Barataria</title>
+	<meta name='robots' content='noindex,follow' />
+
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+	<link rel='stylesheet' href='css/normalize.css' type='text/css' media='all' />
+	<link rel='stylesheet' href='css/style.css' type='text/css' media='all' />
+
+
+</head>
+
+<body>
+
+<div class="contenedor">
+
+	<header>
+		<p><a href="#">I Festival de Cine Ínsula de Barataria</a></p>
+	
+	<ul>
+		<li><a href=#">Inicio</a></li>
+		<li><a href="#">Sección Oficial</a></li>
+		<li><a href="#">Búsqueda de Películas</a></li>
+	</ul>	
+	</header>	
+	
+	<section>
+		<article class="single">
+			
+			<div class="derecha">
+			
+				<h2>Ser y llegar a ser</h2>
+
+				<img  src=https://dl.dropboxusercontent.com/u/77312073/WordPress/imagenes/etre.jpg />			
+			
+			</div>
+			
+			<div class="izquierda">
+			
+				<p class="negrita">Sinopsis:<p>El documental Ser y llegar a ser aborda el tema de la confianza en las criaturas y en su desarrollo, propone el cuestionamiento sobre el aprendizaje y las opciones posibles en relación a ello.</p>
+				<p>La realizadora nos lleva en un viaje por Francia, Inglaterra, Estados Unidos y Alemania (donde es ilegal no ir a la escuela) encontrando padres que eligieron no escolarizar a sus hijos, ni en la escuela, ni en casa y que los dejan aprender libremente con lo que les apasiona.</p>
+				<p>La película es una búsqueda de la verdad acerca del deseo innato de aprender que se sitúa dentro de un tema más amplio que el de la educación, ligado al cambio de nuestras creencias, a la evolución de nuestra sociedad, a la importancia de reapropiarse de su vida y confianza en uno mismo.</p>
+				<p><span class="negrita">Director/a:</span> Clara Bellar</p>
+				<p><span class="negrita">Fecha de estreno:</span> 2015</p>
+				<p><span class="negrita">Nacionalidad:</span> Francia</p>
+				<p><a href="https://www.youtube.com/watch?feature=player_embedded&v=IScd1kG8hU8">Enlace al trailer</p>		
+			</div>
+			
+		</article>
+		
+	</section>
+
+
+	</div> <!--cerramos el contenedor-->
+	<footer>
+		<div>
+			<p>I Festival de Cine Ínsula de Barataria. All wrongs reserved.</p>
+		</div>
+	</footer>
+
+			
+	</body>
+</html>	
