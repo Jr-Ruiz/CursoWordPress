6a2bada3f78e895f4b5e0ecba89cc16d562143a3
José Ramón Ruiz <Jr-Ruiz@users.noreply.github.com>
2016-10-23 17:06:46 +0200

    Create opciones.php

---
+++ a-adir-imagen/opciones.php
@@ -0,0 +1,50 @@
+<?php
+function añadir_soporte_menu(){
+  add_menu_page('Ajustes plugin Añadir Imagen', //Título de la página
+    'Tamaño imagen',	//Título del menú
+    'administrator',	//Rol del que puede acceder
+    'añadir_soporte_settings',	//Id de la página
+    'añadir_soporte_page_settings', //Función que representa la página
+    'dashicons-admin-generic');	//Icono
+}
+
+add_action('admin_menu','añadir_soporte_menu');	
+
+function añadir_soporte_page_settings(){
+	global $ancho, $alto;
+	wp_enqueue_style( 'estilos_admin', plugins_url( 'css/añadir_imagen.css', __FILE__ ) );
+?>
+  <div id="opciones">
+    <h2>Tamaño de la imagen a añadir</h2>
+      <form method="POST" action="options.php">
+        <?php
+          settings_fields('prueba');
+          do_settings_sections('prueba');
+        ?>
+        
+        <div>
+          <label>Anchura de la imagen</label>
+          <input type="text" name="ancho" id ="ancho" value="<?php echo $ancho; ?>" placeholder="220"/>
+        </div>
+        
+        <div>
+          <label>Altura de la imagen</label>
+          <input type="text" name="alto" id ="alto" value="<?php echo $alto; ?>" placeholder="320"/>
+        </div>
+
+        <?php submit_button(); ?>
+        
+      </form>
+    </div>
+
+<?php	
+}
+
+function añadir_soporte_settings(){
+	register_setting('prueba','ancho','intval');
+	register_setting('prueba','alto','intval');
+}
+
+add_Action('admin_init', 'añadir_soporte_settings');
+
+?>
