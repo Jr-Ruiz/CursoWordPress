662f5dee0fb8d8a183f3ff468fb98c26379669d2
José Ramón Ruiz <Jr-Ruiz@users.noreply.github.com>
2016-10-23 17:02:03 +0200

    Create añadir-imagen.php

---
+++ a-adir-imagen/añadir-imagen.php
@@ -0,0 +1,51 @@
+ <?php
+ /**
+	Plugin Name: Añadir Imagen
+	Plugin URI: http://www.joseramonruiz.eu
+	Description: Plugin para añadir una imagen a CPT usando el media uploader
+	Version: 1.0
+	Author: José Ramón Ruiz Rodríguez
+	Author URI: http://www.joseramonruiz.eu
+License: MIT
+**/
+
+$ancho=220;
+$alto=360;
+
+function inicializar(){
+	global $alto, $campo; //RECORDAD que con global hacemos uso de variables declaradas fuera del ámbito de la función
+
+	if(get_option('ancho')){
+		$ancho= get_option('ancho');
+	}
+
+	if(get_option('alto')){
+		$alto= get_option('alto');
+	}
+}
+
+add_action("admin_init", "inicializar");
+
+function añadir_soporte_imagenes(){
+	global $ancho, $alto, $campo;
+	
+	add_theme_support( 'post-thumbnails' );
+	add_image_size( $campo, $ancho, $alto, true );
+}
+
+add_action("admin_init", "añadir_soporte_imagenes");
+
+function my_save_post_function( $post_ID, $post, $update ) {
+	global $post,$ancho, $alto;
+	
+	if($post){
+		$thumbID=get_post_thumbnail_id($post->ID);
+		$imgDestacada=wp_get_attachment_image_src($thumbID,'cartel');
+		$_POST['cartel']=$imgDestacada[0];
+	}
+}
+
+add_action( 'save_post', 'my_save_post_function', 10, 3 );
+
+include 'opciones.php';
+?>
